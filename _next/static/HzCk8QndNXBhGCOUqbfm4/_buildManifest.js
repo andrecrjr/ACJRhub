4b5b204bad5fcdde1f37ff9dac872c8daa798362
andrecrjr <>
2022-10-07 00:21:57 +0000

    Deploying to gh-pages from @ andrecrjr/andrecrjr.github.io@045c997840fbc28252bcd88d06aadc3a9dcf2830 🚀

--- _next/static/HzCk8QndNXBhGCOUqbfm4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9444d814f2f69f99.js"],"/_error":["static/chunks/pages/_error-ddb7477254ab31d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
