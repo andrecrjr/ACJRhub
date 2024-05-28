9747cc044e238706e3ae44e19b958c1c17e5d904
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-05-27 23:26:28 -0300

    - init skillset

--- components/Pages/AboutMe/Skill/index.tsx
+++ components/Pages/AboutMe/Skill/index.tsx
@@ -3,7 +3,16 @@ import React from "react";
 const Skill = () => {
   return <section data-aos="fade-up">
             <h4>Skillset</h4>
-            <p>Work in Progress!</p>
+            <details>
+              <summary>Languages</summary> 
+              <ul>
+                <li>Portuguese: 
+                  Native proficiency. Full command of the language in both reading and writing, with the ability to communicate complex concepts and engage in detailed discussions.
+                </li>
+                <li>English: Intermediate proficiency. Capable of reading and writing with a good understanding of grammar and vocabulary. 
+                  Comfortable engaging in everyday communication and mainly comprehending technical texts</li>
+              </ul>
+            </details>
         </section>;
 };
 
