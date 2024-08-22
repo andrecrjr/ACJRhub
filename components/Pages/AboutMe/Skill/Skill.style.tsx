fe327ec43441a4c3762c974488644f153bed6e3d
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-08-21 22:17:28 -0300

    - update skillset

--- components/Pages/AboutMe/Skill/Skill.style.tsx
+++ components/Pages/AboutMe/Skill/Skill.style.tsx
@@ -1,4 +1,29 @@
-"use client"
-import styled from "styled-components"
-import { ExperienceTitle } from "../Experience/Experience.style"
-export const SkillsetTitle = styled(ExperienceTitle)
+'use client';
+import styled from 'styled-components';
+import { ExperienceTitle } from '../Experience/Experience.style';
+export const SkillsetTitle = styled(ExperienceTitle)``;
+
+export const SkillList = styled.ul`
+	list-style-type: none;
+	display: flex;
+	flex-wrap: wrap;
+	&:not(:first-child) {
+		margin-top: 1.2rem;
+	}
+
+	&:not([data-language]) {
+		li {
+			&:not(:last-child) {
+				&:after {
+					content: '-';
+					padding: 0.77rem;
+				}
+			}
+		}
+	}
+	&[data-language] {
+		li {
+			margin-top: 1rem;
+		}
+	}
+`;
