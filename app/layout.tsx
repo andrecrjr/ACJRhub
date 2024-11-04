4dfdb4dd3286583f921b0c22217a58963f567bdc
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-11-03 23:47:02 -0300

    - update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,33 @@
-import { Metadata } from 'next'
-import Script from 'next/script'
+import type { Metadata } from 'next';
+import Script from 'next/script';
 import { fontPrimaryTheme } from '@/styles/theme';
 import StyledComponentsRegistry from '@/lib/registry';
 import { GlobalThemeProvider } from '@/components/Contexts/ThemeProvider';
- 
+
 export const metadata: Metadata = {
-  title: 'ACJRHUB - Portfolio do Ac Junior',
-  description: `André Carlos Jr's Portfolio to maintain its portfolio projects and works in Front-end development!`,
-  robots: "index, follow"
-}
+	title: 'ACJRHUB - Portfolio do Ac Junior',
+	description: `André Carlos Jr's Portfolio to maintain its portfolio projects and works in Front-end development!`,
+	robots: 'index, follow'
+};
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
+	// Layouts must accept a children prop.
+	// This will be populated with nested layouts or pages
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-
-  return (
-    <html lang="en">
-      <head>
-      <Script
-				strategy="afterInteractive"
-				async
-				src={'https://www.googletagmanager.com/gtag/js?id=G-YCSLXN1PVV'}
-			/>
-			<Script id="google-analytics" strategy="afterInteractive">
-				{process.env.NODE_ENV !== "development" && `
+	return (
+		<html lang="en">
+			<head>
+				<Script
+					strategy="afterInteractive"
+					async
+					src={'https://www.googletagmanager.com/gtag/js?id=G-YCSLXN1PVV'}
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{process.env.NODE_ENV !== 'development' &&
+						`
 						window.dataLayer = window.dataLayer || [];
 						function gtag(){
 							dataLayer.push(arguments);
@@ -35,28 +35,26 @@ export default function RootLayout({
 						gtag('js', new Date());
 						gtag('config', 'G-YCSLXN1PVV');
 				`}
-			</Script>
-          <link rel="preconnect" 
-          href="https://fonts.googleapis.com" />
-					<link
-              rel="preconnect"
-              href="https://fonts.gstatic.com"
-              crossOrigin={''}
-					/>
-					<link href={`${fontPrimaryTheme.urlGoogleFont}`} 
-            rel="stylesheet" />
-					{<meta
+				</Script>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin={''}
+				/>
+				<link href={`${fontPrimaryTheme.urlGoogleFont}`} rel="stylesheet" />
+				{
+					<meta
 						name="google-site-verification"
 						content="google504a726bb71d2ea2"
-					/>}
-      </head>
-      <body>
-        <StyledComponentsRegistry>
-          <GlobalThemeProvider>
-            {children}
-          </GlobalThemeProvider>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  )
+					/>
+				}
+			</head>
+			<body>
+				<StyledComponentsRegistry>
+					<GlobalThemeProvider>{children}</GlobalThemeProvider>
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	);
 }
